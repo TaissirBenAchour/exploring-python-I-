80d2d9bd19185a2f633e2ac99dbb70b96d44894f
TaissirBenAchour <benachourtaissir@gmail.com>
2019-07-04 15:23:42 +0100

    exploring python -I- Jumping and Boundaries

--- pygame_game.py
+++ pygame_game.py
@@ -4,10 +4,12 @@ win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("game window")
 running = True
 x = 50
-y =50
+y =450
 width = 40
 height = 60
 velosity = 5
+isJump = False
+jumpCount = 10
 
 while running:
   pygame.time.delay(100)
@@ -15,15 +17,30 @@ while running:
   pygame.display.update()
 
   keys= pygame.key.get_pressed()
-  if keys[pygame.K_LEFT]:
+  if keys[pygame.K_LEFT] and x >= velosity:
   		x-=velosity
-  if keys[pygame.K_RIGHT]:
+  if keys[pygame.K_RIGHT] and x+width+velosity <= 500:
   		x+=velosity
-  if keys[pygame.K_UP]:
+
+  if not (isJump):
+  	if keys[pygame.K_UP] and y>= velosity:
   		y-=velosity
-  if keys[pygame.K_DOWN]:
+  	if keys[pygame.K_DOWN] and y+height+velosity <= 500:
   		y+=velosity	
-  #win.fill((0,0,0))
+  	if keys[pygame.K_SPACE] :  
+  		isJump = True
+  else : 
+  	if jumpCount >= -10:
+  		neg = 1
+  		if jumpCount<0:
+  			neg = -1
+  		y-=(jumpCount**2)*0.5*neg
+  		jumpCount-=1
+  	else : 
+  		isJump = False
+  		jumpCount = 10
+
+  win.fill((0,0,0))
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       	running = False
